59b0a3ee1f95e051e815a8331ce2123d1d3ae397
Nick <doctor4891@gmail.com>
2021-10-23 15:09:13 +0300

    ping-pong

---
+++ PingPong.sol
@@ -0,0 +1,24 @@
+pragma ton-solidity >= 0.47.0;
+pragma AbiHeader expire;
+/*
+    Create and deploy two contracts with such code
+    Call one of them with send function
+*/
+contract PingPong {
+    uint counter = 0; 
+
+    function send(address dest, uint128 value) public{              
+            dest.transfer(value);        
+            tvm.accept();   
+            counter++;        
+    }
+
+    function count() public returns(uint){
+        tvm.accept();
+        return counter;
+    } 
+
+    receive () external {
+        send(msg.sender, msg.value);
+    }     
+}

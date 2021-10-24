f318656a7a9a0ae2f0f78ffa5896399395cb32d7
Nick <doctor4891@gmail.com>
2021-10-24 12:12:04 +0300

    MassTransactions

---
+++ MassTransactions.sol
@@ -0,0 +1,60 @@
+pragma ton-solidity >= 0.47.0;
+pragma AbiHeader expire;
+
+/*
+This contract for mass transaction generation in TON Blockchain
+Usage: 
+1. Deploy this contract. Add a lot of funds to this contract.
+2. Call function start
+*/
+contract MassTransactions {    
+
+    uint counter = 0;
+    address static m_parent;
+    bool deployed = false;
+    
+    function send(address dest, uint128 value) public{              
+            dest.transfer(value, true, 1);        
+            tvm.accept();   
+            counter++;        
+    }
+
+    function count() public returns(uint){
+        tvm.accept();
+        return counter;
+    }
+
+    function deployer()
+		public		
+		returns (address)
+	{
+        tvm.accept();
+        uint128 value = address(this).balance - 3e8;
+      	return  new MassTransactions{		
+			code: tvm.code(),			
+			value: value,						
+			varInit: {	
+                m_parent: address(this)							
+			}
+		}(); 
+	}
+
+    function start() public{
+        tvm.accept();
+        deployed = true;
+        address child = deployer();
+        send(child, 1e8);
+    }
+
+    function isDeployed() public returns(bool){
+        tvm.accept();
+        return deployed;
+    }
+
+    receive () external {                      
+        if(isDeployed() == false){                   
+            start();           
+        }
+        send(msg.sender, msg.value);          
+    }     
+}

588d0443c530eb836ba7624d79b0d255376ba9fa
Nick <doctor4891@gmail.com>
2021-10-27 20:54:08 +0300

    Update MassTransactions.sol

--- MassTransactions.sol
+++ MassTransactions.sol
@@ -11,8 +11,8 @@ For example, you can get in fld network 100_000 tokens.
 contract MassTransactions {    
 
     uint counter = 0;
-    address static m_parent;
-    bool deployed = false;
+    bool deployed = false;  
+    address static m_parent; 
     
     function send(address dest, uint128 value) public{              
             dest.transfer(value, false, 1);        
@@ -30,15 +30,29 @@ contract MassTransactions {
 		returns (address)
 	{
         tvm.accept();
-        uint128 value = address(this).balance - 5 ton;// more value - more tps
+        (uint128 remainingBalance,) = calculateRemainingBalance();
+        uint128 value = address(this).balance - remainingBalance;       
       	return  new MassTransactions{		
 			code: tvm.code(),			
 			value: value,						
-			varInit: {	
-                m_parent: address(this)							
+			varInit: {
+                m_parent: address(this)	            						
 			}
 		}(); 
 	}
+
+    function calculateRemainingBalance() public returns(uint128 remainingBalance, uint totalContracts){
+        tvm.accept();
+        remainingBalance = 0;
+        totalContracts = 0;
+        int totalAmount = address(this).balance;
+        uint128 diff = 11e6;
+        while(totalAmount > 0) {
+            remainingBalance += diff;
+            totalAmount -= remainingBalance;  
+            totalContracts++;
+        }               
+    }  
 
     function start() public{
         tvm.accept();
